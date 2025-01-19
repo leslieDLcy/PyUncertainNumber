5d6501f2bec82b0e622ae752379070f10fe36b08
leslieDLcy <lesliechen666@outlook.com>
2025-01-19 19:07:45 +0000

    simple modify the test file

--- test/pbox_test.py
+++ test/pbox_test.py
@@ -5,29 +5,24 @@ import numpy as np
 from pyuncertainnumber import pba
 
 # *  ---------------------construction---------------------*#
-with mpl.rc_context({"text.usetex": True}):
-    fig, axs = plt.subplots(nrows=2, ncols=4, figsize=(12, 7), layout="constrained")
+fig, axs = plt.subplots(nrows=2, ncols=4, figsize=(12, 7), layout="constrained")
 
-    # first row
-    pun.normal([0, 1], [2, 3]).display(
-        style="band", ax=axs[0, 0], title="N([0,1],[2,3])"
-    )
-    pun.uniform([0, 1], 3).display(style="band", ax=axs[0, 1], title="U([0,1], 3)")
-    pun.beta([0.7, 1], [3, 4]).display(
-        style="band", ax=axs[0, 2], title="Beta([0.7,1],[3,4])"
-    )
-    pun.gamma([5, 6], 2).display(style="band", ax=axs[0, 3], title="Gamma([5,6],2)")
+# first row
+pun.normal([0, 1], [2, 3]).display(style="band", ax=axs[0, 0], title="N([0,1],[2,3])")
+pun.uniform([0, 1], 3).display(style="band", ax=axs[0, 1], title="U([0,1], 3)")
+pun.beta([0.7, 1], [3, 4]).display(
+    style="band", ax=axs[0, 2], title="Beta([0.7,1],[3,4])"
+)
+pun.gamma([5, 6], 2).display(style="band", ax=axs[0, 3], title="Gamma([5,6],2)")
 
-    # second row
-    pun.lognormal([2, 3], [1, 5]).display(
-        style="band", ax=axs[1, 0], title="Lognormal([2,3],[1,5])"
-    )
-    pun.expon([0.4, 0.6]).display(style="band", ax=axs[1, 1], title="Exp([0.4, 0.6])")
-    pun.chi2([20, 50]).display(style="band", ax=axs[1, 2], title="Chisquared([20, 50])")
-    pun.cauchy([1, 100], 1).display(
-        style="band", ax=axs[1, 3], title="Cauchy([1,100], 1)"
-    )
-    # plt.show()
+# second row
+pun.lognormal([2, 3], [1, 5]).display(
+    style="band", ax=axs[1, 0], title="Lognormal([2,3],[1,5])"
+)
+pun.expon([0.4, 0.6]).display(style="band", ax=axs[1, 1], title="Exp([0.4, 0.6])")
+pun.chi2([20, 50]).display(style="band", ax=axs[1, 2], title="Chisquared([20, 50])")
+pun.cauchy([1, 100], 1).display(style="band", ax=axs[1, 3], title="Cauchy([1,100], 1)")
+# plt.show()
 
 # *  ---------------------aggregation---------------------*#
 
