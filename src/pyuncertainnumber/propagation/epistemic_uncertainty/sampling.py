c92fafdcdf4e0f22e626f06a99a86c0018150513
ioannaioan <ii54250@gmail.com>
2025-02-05 22:55:44 +0000

    Update sampling.py
    updated the description of sampling. I have also updated the code to be able to sample from bathtub shaped input distributions.

--- src/pyuncertainnumber/propagation/epistemic_uncertainty/sampling.py
+++ src/pyuncertainnumber/propagation/epistemic_uncertainty/sampling.py
@@ -1,14 +1,14 @@
 import numpy as np
-from scipy.stats import qmc
+from scipy.stats import qmc, beta
 import tqdm
-from typing import Callable
+from typing import Callable, Union
 from pyuncertainnumber.propagation.utils import Propagation_results
 
 
 def index_to_bool_(index: np.ndarray, dim=2):
     """Converts a vector of indices to an array of boolean pairs for masking.
 
-    Args:
+    args:
         index: A NumPy array of integer indices representing selected elements or categories.
                 The values in `index` should be in the range [0, dim-1].
         dim (scalar): The number of categories or dimensions in the output boolean array. Defaults to 2.
@@ -21,8 +21,8 @@ def index_to_bool_(index: np.ndarray, dim=2):
           - the argument `dim` will specify the function mapping of variables to be propagated.
           - If dim > 2,  e.g. (2,0,1,0) the array of booleans is [(0,0,1),(1,0,0),(0,1,0),(1,0,0)].
 
-    Returns:
-      - A NumPy array of boolean pairs representing the mask.
+    returns:
+      A NumPy array of boolean pairs representing the mask.
     """
 
     index = np.asarray(index, dtype=int)
@@ -33,10 +33,11 @@ def sampling_method(
     x: np.ndarray,
     f: Callable,
     results: Propagation_results = None,
-    n_sam: int = 500,
     method="monte_carlo",
-    save_raw_data="no",
+    b: Union[float, np.ndarray] = 1.0,
+    n_sam: int = 500,
     endpoints=False,
+    save_raw_data="no",
 ) -> Propagation_results:  # Specify return type
     """sampling of intervals
 
@@ -45,51 +46,75 @@ def sampling_method(
                             the two columns define its lower and upper bounds (interval).
         f (Callable): A callable function that takes a 1D NumPy array of input values and returns the
                         corresponding output(s). Can be None, in which case only samples are generated.
-        n_sam (int): The number of samples to generate for the chosen sampling method.
+        results (Propagation_results, optional): An object to store propagation results.
+                                            Defaults to None, in which case a new
+                                            `Propagation_results` object is created.
         method (str, optional): The sampling method to use. Choose from:
                                  - 'monte_carlo': Monte Carlo sampling (random sampling from uniform distributions)
                                  - 'latin_hypercube': Latin Hypercube sampling (stratified sampling for better space coverage)
                                 Defaults to 'monte_carlo'.
+        b (float or np.ndarray): The shape parameter(s) for the bathtub distribution(s).
+                                  If a float, the same shape parameter is used for all inputs.
+                                  If an np.ndarray, it should have the same length as the number of input variables,
+                                  providing a separate shape parameter for each input. Default: 1.0 (uniform distribution).
+        n_sam (int): The number of samples to generate for the chosen sampling method.
         endpoints (bool, optional): If True, include the interval endpoints in the sampling.
                                     Defaults to False.
-        save_raw_data (str, optional): Whether to save raw data. Options: 'yes', 'no'.
-                                        Defaults to 'no'.
+        save_raw_data (str, optional): Acts as a switch to enable or disable the storage of raw input data when a function (f) 
+          is not provided.
+          - 'no': Returns an error that no function is provided.
+          - 'yes': Returns the full arrays of unique input combinations.
 
     signature:
-        sampling_method(x:np.ndarray, f:Callable, n_sam:int, method ='montecarlo', endpoints=False, results:dict = None, save_raw_data = 'no') -> dict of np.ndarrays
+        sampling_method(x:np.ndarray, f:Callable, results:Propagation_results = None,  method ='montecarlo', b=1, n_sam:int,
+        endpoints=False,  save_raw_data = 'no') -> Propagation_results
 
     note:
-        - The function assumes that the na in `x` represent uniform distributions.
-        - If the `f` function returns multiple outputs, the `all_output` array will be 2-dimensional y and x for all x samples.
+        - The `f` function may return multiple outputs.
+        - For the Monte carlo method, sampling with bathtub distributions allows for more flexible exploration of the input space
+          compared to uniform distributions.
+        - Bathtub distributions are not directly applicable to the latin hypercube which samples from uniform input distributions. 
+        - To account for the extreme values of each interval, sampling methods are combined with the endpoints method. 
+        - Overall, the sampling method is expected to yield non-conservative results if used to propagate input
+          intervals through a black-box model.
 
     returns:
-        dict: A dictionary containing the results:
-            - 'bounds': An np.ndarray of the bounds for each output parameter (if f is not None).
-            - 'min': A dictionary for lower bound results (if f in not None)
-                - 'x': Input values that produced the miminum output value(s) (if f is not None).
-                - 'f': Minimum output value(s) (if f is not None).
-            - 'max':  A dictionary for upper bound results (if f in not None)
-                - 'x': Input values that produced the maximum output value(s) (if f is not None).
-                - 'f': Maximum output value(s) (if f is not None).
-            - 'raw_data': A dictionary containing raw data (if save_raw_data is 'yes'):
-                - 'x': All generated input samples.
-                - 'f': Corresponding output values for each input sample.
+       `Propagation_results` object(s) containing:
+            - 'un': UncertainNumber object(s) to characterise the interval(s) of the output(s).
+            - 'raw_data' (dict): Dictionary containing raw data shared across output(s):
+                    - 'x' (np.ndarray): Input values.
+                    - 'f' (np.ndarray): Output values.
+                    - 'min' (np.ndarray): Array of dictionaries, one for each output,
+                              containing 'f' and corresponding 'x' for the minimum of that output.
+                    - 'max' (np.ndarray): Array of dictionaries, one for each output,
+                              containing 'f' and corresponding 'x' for the maximum of that output.
+                    - 'bounds' (np.ndarray): 2D array of lower and upper bounds for each output.
+
+    raises:
+        ValueError if no function is provided and save_raw_data is 'no'.
+
     example:
         >>> x = np.array([[1, 2], [3, 4], [5, 6]])  # Define input intervals
         >>> f = lambda x: x[0] + x[1] + x[2]  # Define the function
-        >>> y = sampling_method(x, f, n_sam=500, method='monte_carlo', endpoints=False, save_raw_data='no')
+        >>> y = sampling_method(x, f,  method='monte_carlo', b=1,  n_sam=500, endpoints=False, save_raw_data='no')
     """
     if results is None:
         results = Propagation_results()  # Create an instance of Propagation_results
-
-    print(f"Total number of input combinations for the {method} method: {n_sam}")
 
     m = x.shape[0]
     lo = x[:, 0]
     hi = x[:, 1]
 
+    # Ensure b is an array of the correct length
+    if isinstance(b, float):
+        b_arr = np.full(m, b)
+    else:
+        b_arr = b
+        
     if method == "monte_carlo":
-        X = lo + (hi - lo) * np.random.rand(n_sam, m)
+        X = np.zeros((n_sam, m))
+        for i in range(m):
+            X[:, i] = lo[i] + (hi[i] - lo[i]) * beta.rvs(1/b_arr[i], 1/b_arr[i], size=n_sam)
     elif method == "latin_hypercube":
         sampler = qmc.LatinHypercube(m)
         X = lo + (hi - lo) * sampler.random(n_sam)
@@ -100,9 +125,7 @@ def sampling_method(
 
     if endpoints:
         m = x.shape[0]
-        Total = (
-            2**m
-        )  # Total number of endpoint combination for the give x input variables
+        Total = (2**m )  # Total number of endpoint combination for the give x input variables
         # Initialize array for endpoint combinations
         X_end = np.zeros((Total, m))
         for j in range(Total):
@@ -114,7 +137,7 @@ def sampling_method(
         X = np.vstack([X, X_end])
 
     if f is not None:  # Only evaluate if f is provided
-        all_output = np.array([f(xi) for xi in tqdm.tqdm(X, desc="Evaluating samples")])
+        all_output = np.array([f(xi) for xi in tqdm.tqdm(X, desc="Function evaluations")])
 
         try:
             num_outputs = len(all_output[0])
@@ -183,5 +206,5 @@ def sampling_method(
 # x_bounds = np.array([[1, 2], [3, 4], [5, 6]])
 # n=20
 # # Call the method
-# y = sampling_method(x_bounds,f=f, n_sam=n, method  ='latin_hypercube' , endpoints= True, save_raw_data = 'yes')
-# y.print()
+# y = sampling_method(x_bounds,f=f,  method  ='monte_carlo' , n_sam=n, endpoints= False, save_raw_data = 'yes')
+# print(y.raw_data['bounds'])
