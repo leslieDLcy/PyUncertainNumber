d803b5b52e198e8054997c87b5c807e4be02ad41
ioannaioan <ii54250@gmail.com>
2025-02-05 11:59:48 +0000

    updated cauchy method
    Better description and minor changes in the code for the user to have better control on the optimisation function.

--- src/pyuncertainnumber/propagation/epistemic_uncertainty/endpoints_cauchy.py
+++ src/pyuncertainnumber/propagation/epistemic_uncertainty/endpoints_cauchy.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tqdm
-from typing import Callable
+from typing import Callable, Union
 from scipy.optimize import brentq
 from pyuncertainnumber.propagation.utils import Propagation_results
 
@@ -10,9 +10,14 @@ def cauchydeviates_method(
     f: Callable,
     results: Propagation_results = None,
     n_sam: int = 500,
+    xtol: Union[float, np.ndarray]  = 2e-12,
+    rtol: Union[float, np.ndarray]  = 8.881784197001252e-16,
+    maxiter: Union[float, np.ndarray]  = 100,
+    min_y0: Union[float, np.ndarray] = 1e-6,   
     save_raw_data="no",
 ) -> Propagation_results:  # Specify return type
-    """This method propagates intervals through a balck box model with the endpoint Cauchy deviate method. It is an approximate method, so the user should expect non-identical results for different runs.
+    """This method propagates intervals through a balck box model with the Cauchy deviate method. 
+    It is an approximate method, so the user should expect non-identical results for different runs.
 
     args:
         x (np.ndarray): A 2D NumPy array representing the intervals for each input variable.
@@ -21,17 +26,52 @@ def cauchydeviates_method(
                         and returns a single output value or an array of output values.
                         Can be None, in which case only the Cauchy deviates (x) and the
                         maximum Cauchy deviate (K) are returned.
-        results_class (Propagation_results): The class to use for storing results (defaults to Propagation_results).
         n_sam (int): The number of samples (Cauchy deviates) to generate for each input variable (defaults 500 samples).
-        save_raw_data (str, optional): Whether to save raw data. Defaults to 'no'.
-                                        Currently not supported by this method.
+        results (Propagation_results): The class to use for storing results (defaults to Propagation_results).
+        xtol (float): The absolute tolerance(s) for the root-finding algorithm (brentq). 
+                      If a float, the same xtol is used for all outputs.If an np.ndarray, it should have the same length 
+                      as the output of f, providing an absolute tolerance for brentq for each output. Default: 2e-12.
+        rtol (float): The relative tolerance(s) for the root-finding algorithm (brentq). 
+                      If a float, the same rtol is used for all outputs.If an np.ndarray, it should have the same length 
+                      as the output of f, providing a relative tolerance for brentq for each output. Default: 8.88e-16.
+        maxiter (int): The maximum number(s) of iterations for the root-finding algorithm (brentq). 
+                       If a float, the same maxiter is used for all outputs.If an np.ndarray, it should have the same length 
+                       as the output of f, providing a maximum number of iterations for brentq for each output. Default: 100.
+        min_y0 (float, or np.ndarray): The lower bound(s) for the interval(s) in the brentq root-finding algorithm.
+                                       If a float, the same lower bound is used for all outputs.
+                                       If an np.ndarray, it should have the same length as the output of f,
+                                       providing a separate lower bound for each output. Default: 1e-6.
+        save_raw_data (str, optional): Acts as a switch to enable or disable the storage of raw input data when a function (f) 
+          is not provided.
+          - 'no': Returns an error that no function is provided.
+          - 'yes': Returns the full arrays of unique input combinations.
 
     signature:
         cauchydeviate_method(x: np.ndarray, f: Callable, results_class = Propagation_results,
-                        n_sam: int, save_raw_data='no') -> dict
+                        n_sam: int, save_raw_data='no') -> Propagation_results
+
+    notes:
+        The interval for the `brentq` root-finding algorithm is chosen heuristically.
+        The lower bound is set to 1e-6, assuming the root (representing the change
+        in the function's value) is positive and not exactly zero.  The upper bound
+        is calculated as half the maximum absolute difference observed between the
+        function's value at the central point and the values at the Cauchy deviate
+        samples (`max(filtered_deltaF) / 2` or `max(filtered_deltaF_i) / 2`).
+        This choice is based on the assumption that the actual change in the 
+        function's value is likely to be smaller than the largest observed change.
+
+        Users should be aware that these choices are heuristics and might need to be
+        adjusted depending on the specific function and problem being solved. If
+        `brentq` fails to converge, consider experimenting with different interval
+        bounds.  For example, if the function is highly sensitive to input changes,
+        the upper bound might be too large, and dividing by a larger number (e.g.,
+        4 or 8 instead of 2) might be necessary.  Alternatively, if the root is
+        expected to be very close to zero, the lower bound might need to be
+        reduced.
 
     returns:
         A PropagationResult object containing the results.
+            - 'un': UncertainNumber object(s) representing the uncertainty in the output(s) of the function.
             - 'raw_data': A dictionary containing raw data (if f is None):
                 - 'x': Cauchy deviates (x).
                 - 'K': Maximum Cauchy deviate (K).
@@ -42,6 +82,9 @@ def cauchydeviates_method(
                 - 'max':  A dictionary for upper bound results (if f is not None).
                     - 'f': Maximum output value(s).
                     - 'x': None (as input values corresponding to min/max are not tracked in this method).
+
+    raises:
+        ValueError if no function is provided and save_raw_data is 'no'.
 
     example:
         >>> f = lambda x: x[0] + x[1] + x[2]  # Example function
@@ -76,9 +119,13 @@ def cauchydeviates_method(
 
             def Z(Del):
                 return n_sam / 2 - np.sum(1 / (1 + (deltaF / Del) ** 2))
+            
+            mask = np.isnan(deltaF)
+            filtered_deltaF = deltaF[~mask]
 
             # Use a small value for the lower bound
-            zRoot = brentq(Z, 1e-6, max(deltaF) / 2)
+            zRoot = brentq(Z, min_y0, max(filtered_deltaF) / 2, 
+                                              xtol =  xtol, rtol = rtol,maxiter=maxiter )
             min_candidate = np.array([ytilde - zRoot])
             max_candidate = np.array([ytilde + zRoot])
             bounds = np.array([min_candidate[0], max_candidate[0]])
@@ -92,6 +139,27 @@ def cauchydeviates_method(
             deltaF = np.zeros((n_sam, len_y))
             min_candidate = np.empty(len_y)
             max_candidate = np.empty(len_y)
+
+            if isinstance(min_y0, float):
+                min_y0_arr = np.full(len_y, min_y0)
+            else:
+                min_y0_arr = min_y0
+            
+            if isinstance(xtol, float):
+                xtol_arr = np.full(len_y, xtol)
+            else:
+                xtol_arr = xtol
+            
+            if isinstance(rtol, float):
+                rtol_arr = np.full(len_y, rtol)
+            else:
+                rtol_arr = rtol
+            
+            if isinstance(maxiter, float):
+                maxiter_arr = np.full(len_y, maxiter)
+            else:
+                maxiter_arr = maxiter
+
 
             for k in tqdm.tqdm(range(n_sam), desc="Calculating Cauchy deviates"):
                 r = np.random.rand(x.shape[0])
@@ -113,7 +181,8 @@ def cauchydeviates_method(
 
                 try:  # Handle potential errors in brentq
                     # Use a small value for the lower bound
-                    zRoot = brentq(Z, 1e-6, max(filtered_deltaF_i) / 2)
+                    zRoot = brentq(Z, min_y0_arr[i], max(filtered_deltaF_i) / 2, 
+                                              xtol =  xtol_arr[i], rtol = rtol_arr[i],maxiter=maxiter_arr[i] )
                 except ValueError:
                     print(f"Warning: brentq failed for output {i}. Using 0 for zRoot.")
                     zRoot = 0  # Or handle the error in another way
@@ -165,7 +234,6 @@ def cauchydeviates_method(
 # x_bounds = np.array([[1, 2], [3, 4], [5, 6]])
 # n=50
 # # Call the method
-# y = cauchydeviates_method(x_bounds,f=None, n_sam=n, save_raw_data= 'yes')
-
-# # print the results
-# y.print()
+# y = cauchydeviates_method(x_bounds,f=f, n_sam=n, save_raw_data= 'yes')
+# # # print the results
+# # y.summary()
