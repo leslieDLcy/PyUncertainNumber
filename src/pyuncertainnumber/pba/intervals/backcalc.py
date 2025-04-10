f27800245e03808402f8f4109cb9572dec0f644b
leslieDLcy <lesliechen666@outlook.com>
2025-04-10 14:38:04 +0100

    interval_backcalc

---
+++ src/pyuncertainnumber/pba/intervals/backcalc.py
@@ -0,0 +1,47 @@
+from .number import Interval
+from .methods import log, exp
+
+"""interval backcalculation"""
+
+# * ------------------ backcalculation ------------------ *#
+
+
+def backcalc(a, c) -> Interval:
+    """backcalculation operation for the Interval object
+
+    signature:
+        backcalc(a: Interval, c: Interval) -> Interval
+        example: B = backcalc(A, C) # A + B = C
+    """
+    if a.scalar & c.scalar:
+        lo = c.lo - a.lo
+        hi = c.hi - a.hi
+        return Interval(lo, hi)
+    else:
+        raise NotImplementedError(
+            "backcalculation is not yet implemented for this type of operation"
+        )
+
+
+def factor(a, c):
+    """A * B = C
+
+    signature:
+        factor(a: Interval, c: Interval) -> Interval
+        example: B = factor(A, C) # A * B = C
+    """
+
+    if a.scalar & c.scalar:
+        return exp(backcalc(log(a), log(c)))
+    else:
+        raise NotImplementedError(
+            "backcalculation is not yet implemented for this type of operation"
+        )
+
+
+# * ------------------ controled backcalculation ------------------ *#
+
+
+def control_bcc(a, c):
+    """controlled backcalculation solution for A + B = C"""
+    return -1 * backcalc(c, a)
