5e7109dbfac478cc81b1bd155407ad85f19c9054
leslieDLcy <lesliechen666@outlook.com>
2025-07-07 14:57:57 +0100

    added interval-> pbox in `convert` function

--- src/pyuncertainnumber/pba/operation.py
+++ src/pyuncertainnumber/pba/operation.py
@@ -40,9 +40,12 @@ def convert(un):
     from .pbox_abc import Pbox
     from .dss import DempsterShafer
     from .distributions import Distribution
+    from .intervals.number import Interval
 
     if isinstance(un, Pbox):
         return un
+    elif isinstance(un, Interval):
+        return un.to_pbox()
     elif isinstance(un, Distribution):
         return un.to_pbox()
     elif isinstance(un, DempsterShafer):
@@ -139,6 +142,19 @@ def adec(a, c):
         if b[i] > r[i]:
             raise ValueError("Math Problem: couldn't deconvolve")
     return Staircase(left=b, right=r)
+
+
+# * --------------- independent ops --------------- *#
+
+
+def i_mul(a, b):
+    """bivariate independent multiplication of pboxes"""
+
+    if isinstance(a, Number) or isinstance(b, Number):
+        return a * b
+    else:  # 2 pboxes
+        a, b = convert(a), convert(b)
+        return a.mul(b, dependency="i")
 
 
 # * --------------- vectorisation --------------- *#
@@ -373,6 +389,94 @@ class Matrix:
 
     def tanh(self):
         return Matrix([[x.tanh() for x in row] for row in self.rows])
+
+
+# * --------------- independet Vector and Matrix for Pbox operations --------------- *#
+class iVector(Vector):
+    """Independent Vector of Pboxes"""
+
+    def __init__(self, components):
+        super().__init__(components)
+
+    def __add__(self, other):
+        if isinstance(other, Vector | list):
+            if len(self) != len(other):
+                raise ValueError("Vectors must be the same length")
+            return iVector([isum(a, b) for a, b in zip(self, other)])
+        return NotImplemented
+
+    def __matmul__(self, other):
+        if isinstance(other, Vector | list):
+            if len(self) != len(other):
+                raise ValueError("Vectors must be the same length")
+            return isum(x.mul(y, dependency="i") for x, y in zip(self, other))
+
+        elif isinstance(other, Matrix):
+            # Vector @ Matrix: treat self as row vector, multiply by matrix
+            n = len(self)
+            m_rows, m_cols = other.shape()
+            if n != m_rows:
+                raise ValueError(
+                    "Vector length must match matrix rows (row vector @ matrix)"
+                )
+            result = []
+            for col in zip(*other.rows):
+                result.append(
+                    isum(v.mul(c, dependency="i") for v, c in zip(self.components, col))
+                )
+            return Vector(result)
+
+        return NotImplemented
+
+
+class iMatrix(Matrix):
+    """Independent Matrix of Pboxes"""
+
+    def __init__(self, rows):
+        super().__init__(rows)
+
+    def __add__(self, other):
+        if isinstance(other, iMatrix):
+            if self.shape() != other.shape():
+                raise ValueError("Matrices must have the same shape")
+            return iMatrix(
+                [
+                    [isum(a, b) for a, b in zip(row1, row2)]
+                    for row1, row2 in zip(self.rows, other.rows)
+                ]
+            )
+        return NotImplemented
+
+    def __matmul__(self, other):
+        if isinstance(other, Vector):
+            # Matrix @ Vector
+            m, n = self.shape()
+            if n != len(other):
+                raise ValueError("Matrix columns must match vector length")
+            return iVector(
+                [
+                    isum(
+                        r[i].mul(other.components[i], dependency="i") for i in range(n)
+                    )
+                    for r in self.rows
+                ]
+            )
+
+        elif isinstance(other, Matrix):
+            # Matrix @ Matrix
+            m1, n1 = self.shape()
+            m2, n2 = other.shape()
+            if n1 != m2:
+                raise ValueError("Incompatible shapes for matrix multiplication")
+            result_rows = []
+            for row in self.rows:
+                result_row = []
+                for col in zip(*other.rows):
+                    result_row.append(isum(r * c for r, c in zip(row, col)))
+                result_rows.append(result_row)
+            return iMatrix(result_rows)
+
+        return NotImplemented
 
 
 ### use this later as `x.mul(y, dependency="i")` affects generality with other data types
