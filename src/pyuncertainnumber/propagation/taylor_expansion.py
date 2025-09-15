44791d9067d48569b1636626ecbe9f78ec5555d3
leslieDLcy <lesliechen666@outlook.com>
2025-09-15 17:17:53 +0100

    added taylor expansion for moment propagation

---
+++ src/pyuncertainnumber/propagation/taylor_expansion.py
@@ -0,0 +1,62 @@
+import jax
+import jax.numpy as jnp
+
+
+""" Taylor expansions for the moments of functions of random variables """
+
+
+def taylor_expansion_method(func, mean, *, var=None, cov=None):
+    """Performs uncertainty propagation using the Taylor expansion method.
+
+    args:
+        func: function to propagate uncertainty through
+        mean: mean of the input random variable (scalar or vector)
+        var: variance of the input random variable (scalar only)
+        cov: covariance matrix of the input random vector (vector only)
+
+    returns:
+        mu_f: mean of the output random variable through the function
+        var_f: variance of the output random variable through the function
+
+    note:
+        Currently it only supports scalar-output functions. Also, for multivariate function, the
+        calling signature is assumed to be func(x) where x is a 1D array, i.e. func: R^n -> R, the vec style.
+        For best compatibility to work with derivatives, the `func` is better written in jax.numpy.
+
+    """
+    if mean.ndim == 1:  # random vector
+        return taylor_expansion_method_vector(func, mean, cov)
+    elif mean.ndim == 0:  # scalar random variable
+        return taylor_expansion_method_scalar(func, mean, var)
+
+
+def taylor_expansion_method_scalar(func, mean, var) -> tuple:
+    """For scalar random variable only"""
+
+    # gradient
+    d1f = jax.grad(func)(mean)
+
+    # second-order
+    H = jax.hessian(func)
+    d2f = jnp.diag(H(mean))
+
+    mu_f = func(mean) + jnp.dot(d2f / 2.0, var)
+    var_f = jnp.dot(d1f**2, var) - 1 / 4 * jnp.dot(d2f**2, var**2)
+
+    return mu_f, var_f
+
+
+def taylor_expansion_method_vector(func, mean, cov) -> tuple:
+    """For random vector only"""
+
+    # gradient
+    d1f = jax.grad(func)(mean)
+
+    # second-order
+    H = jax.hessian(func)
+
+    mu_f = func(mean) + 1 / 2 * jnp.trace(H(mean) @ cov)
+
+    var_f = d1f @ cov @ d1f + 1 / 2 * jnp.trace(H(mean) @ cov @ H(mean) @ cov)
+
+    return mu_f, var_f
