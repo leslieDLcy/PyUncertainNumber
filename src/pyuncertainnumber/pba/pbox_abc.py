af9ecfe92f7997701b7b01197af6c48945e4523e
leslieDLcy <lesliechen666@outlook.com>
2025-09-16 14:22:32 +0100

    added tan() for Pbox objects

--- src/pyuncertainnumber/pba/pbox_abc.py
+++ src/pyuncertainnumber/pba/pbox_abc.py
@@ -647,6 +647,8 @@ class Staircase(Pbox):
             return self.sin()
         if ufunc is np.cos:
             return self.cos()
+        if ufunc is np.tan:
+            return self.tan()
         if ufunc is np.tanh:
             return self.tanh()
         if ufunc is np.exp:
@@ -1014,6 +1016,12 @@ class Staircase(Pbox):
         from .intervals.methods import cos
 
         itvls = cos(self.to_interval())
+        return simple_stacking(itvls)
+
+    def tan(self):
+        from .intervals.methods import tan
+
+        itvls = tan(self.to_interval())
         return simple_stacking(itvls)
 
     def tanh(self):
